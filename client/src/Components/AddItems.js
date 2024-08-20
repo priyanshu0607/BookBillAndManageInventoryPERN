f1fc0869661ab36a664d2bce93d7f69b0ea8a8fa
Priyanshu Sharma <63731567+priyanshu0607@users.noreply.github.com>
2024-08-20 12:19:56 +0530

    Update AddItems.js

--- client/src/Components/AddItems.js
+++ client/src/Components/AddItems.js
@@ -16,7 +16,7 @@ const AddItems = ({ getTotalAmount, getSelectedItems }) => {
     useEffect(() => {
         const fetchItems = async () => {
             try {
-                const response = await axios.get('http://localhost:3000/api/bill/items');
+                const response = await axios.get('https://book-bill-and-manage-inventory-pern.vercel.app/api/bill/items');
                 setItems(response.data.rows);
             } catch (error) {
                 console.error('Error fetching items:', error);
