1854ae95d3e4103aef02623714eb0bc3348f7a2f
Priyanshu Sharma <63731567+priyanshu0607@users.noreply.github.com>
2024-08-20 12:30:25 +0530

    Update Login_Page.js

--- client/src/Components/Login_Page.js
+++ client/src/Components/Login_Page.js
@@ -15,7 +15,7 @@ const Login = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await fetch('http://localhost:3000/api/users/login', {
+      const response = await fetch('https://book-bill-and-manage-inventory-pern.vercel.app/api/users/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password })
